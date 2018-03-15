2a26ce6c2d6a8936d2fa130253e65ace7cb3ba48
alexbarrera <alejandro.barrera@duke.edu>
2018-03-15 13:28:29 -0400

    Change understimated Prius mileage range

--- vehicles.py
+++ vehicles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Define the class of vehicle Car
-class Car:
+class Car(object):
     def __init__(self, doors=3, mpg=30):
         self.wheels = 4
         self.doors = doors
@@ -13,5 +13,7 @@ class Car:
     
 # Define a Prius Car
 class Prius(Car):
-    def __init__(self, doors=4, mpg=50):
+    def __init__(self, doors=4, mpg=52.4):
         super(Prius, self).__init__(doors=doors, mpg=mpg)
+
+        
